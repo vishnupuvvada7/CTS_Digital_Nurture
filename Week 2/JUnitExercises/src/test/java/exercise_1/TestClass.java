3317538dc08b3937a4b6ef515b4c17255ad3b3b5
vishnu puvvada <Vishnupuvvada7@gmail.com>
2025-06-28 14:19:54 +0530

    Junit exercises

---
+++ Week 2/JUnitExercises/src/test/java/exercise_1/TestClass.java
@@ -0,0 +1,13 @@
+package exercise_1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("unused")
+public class TestClass {
+	@Test
+	void sampleTest() throws Exception {
+		
+	}
+}
---
+++ Week 2/JUnitExercises/src/test/java/exercise_3/AssertionsTest.java
@@ -0,0 +1,21 @@
+package exercise_3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AssertionsTest {
+
+    @Test
+    public void testAssertions() {
+    	
+        assertEquals(5, 2 + 3);
+
+        assertTrue(5 > 3);
+
+        assertFalse(5 < 3);
+
+        assertNull(null);
+
+        assertNotNull(new Object());
+    }
+}
