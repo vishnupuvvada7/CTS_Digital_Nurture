7797209428331f3babe1d6006e3456e17d6bcaa8
vishnu puvvada <Vishnupuvvada7@gmail.com>
2025-06-28 16:48:53 +0530

    exercise 4

---
+++ Week 2/JUnitExercises/src/test/java/exercise_4/CalculatorTest.java
@@ -0,0 +1,49 @@
+package exercise_4;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+        System.out.println("Setup completed");
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Teardown completed");
+    }
+
+    @Test
+    public void testAddition() {
+        // Arrange
+        int a = 10;
+        int b = 5;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void testSubtraction() {
+        // Arrange
+        int a = 10;
+        int b = 5;
+
+        // Act
+        int result = calculator.subtract(a, b);
+
+        // Assert
+        assertEquals(5, result);
+    }
+}
